d8f5de8ac182dcb26295f7cc284f9ea9a4975197
NichtJens <nichtjens@gmail.com>
2017-01-06 21:43:53 -0800

    added _tm_ in front of stacks and initial state to avoid name clashes in the wrapped class

--- timemachine.py
+++ timemachine.py
@@ -17,8 +17,8 @@ def redo(obj, *args, **kwargs):
 def altering(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
-        self.redostack = []
-        self.undostack.append((func, args, kwargs))
+        self._tm_redostack = []
+        self._tm_undostack.append((func, args, kwargs))
         func(self, *args, **kwargs)
     return wrapper
 
@@ -33,12 +33,12 @@ def tm_init(self, *args, **kwargs):
     cls = type(self)
     cls.__original_init__(self, *args, **kwargs)
 
-    assert not hasattr(self, "redostack")
-    assert not hasattr(self, "undostack")
-    assert not hasattr(self, "original")
-    self.redostack = []
-    self.undostack = []
-    self.original  = deepcopy(self)
+    assert not hasattr(self, "_tm_redostack")
+    assert not hasattr(self, "_tm_undostack")
+    assert not hasattr(self, "_tm_initial_state")
+    self._tm_redostack = []
+    self._tm_undostack = []
+    self._tm_initial_state  = deepcopy(self)
 
     assert not hasattr(self, "__reset__")
     assert not hasattr(self, "__undo__")
@@ -50,32 +50,32 @@ def tm_init(self, *args, **kwargs):
 
 
 def tm_reset(self):
-    self.__dict__.update(self.original.__dict__)
+    self.__dict__.update(self._tm_initial_state.__dict__)
 
 
 def tm_undo(self):
-    if not self.undostack:
+    if not self._tm_undostack:
         return
 
-    undostack = self.undostack
-    redostack = self.redostack
+    undostack = self._tm_undostack
+    redostack = self._tm_redostack
 
     cmd = undostack.pop()
     redostack.append(cmd)
 
     self.__reset__()
-    self.undostack = undostack
-    self.redostack = redostack
+    self._tm_undostack = undostack
+    self._tm_redostack = redostack
     for func, args, kwargs in undostack:
         func(self, *args, **kwargs)
 
 
 def tm_redo(self):
-    if not self.redostack:
+    if not self._tm_redostack:
         return
 
-    cmd = self.redostack.pop()
-    self.undostack.append(cmd)
+    cmd = self._tm_redostack.pop()
+    self._tm_undostack.append(cmd)
 
     func, args, kwargs = cmd
     func(self, *args, **kwargs)
@@ -123,11 +123,11 @@ print ex
 ex.up()
 print ex
 
-print ex.undostack
-print ot.undostack
+print ex._tm_undostack
+print ot._tm_undostack
 
-print ex.original
-print ot.original
+print ex._tm_initial_state
+print ot._tm_initial_state
 
 print ex
 print "reset"
@@ -153,8 +153,8 @@ print ex
 undo(ex)
 print ex
 
-print ex.redostack
-print ot.redostack
+print ex._tm_redostack
+print ot._tm_redostack
 
 print "redo"
 redo(ex)
@@ -186,9 +186,9 @@ print ex
 
 print "undo+new_cmd"
 undo(ex)
-print ex.redostack
+print ex._tm_redostack
 ex.down()
-print ex.redostack
+print ex._tm_redostack
 
 
 
